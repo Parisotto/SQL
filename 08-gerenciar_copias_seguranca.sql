1e1cd860a596b00b4a2b48f70e73c175655078a9
Edson Parisotto <parisotto@gmail.com>
2026-04-28 08:50:51 -0300

    Add files via upload

---
+++ 08-gerenciar_copias_seguranca.sql
@@ -0,0 +1,27 @@
+-- dump - backup do BD
+
+-- Server > Data Export 
+
+-- Selecione o Schea (BD) e as tabelas
+
+-- Dump Structure and Data
+
+-- Export to Self-Contained File
+
+-- Escolha a pasta de destino e, se quiser, mude o nome do arquivo (melhor deixar o automático)
+
+-- guia Export Progress 
+
+-- Botão Start Export
+
+
+-- XAMPP > Shell 
+-- mysql -u root -p
+-- show databases;
+-- status
+-- Current database: <vazio>
+-- uses fateczs;
+-- status
+-- show tables;
+-- describe estudantes;
+
